21af22b26163d6da793267c15927df374897ea7d
daynlight <danielstodulski000@gmail.com>
2025-10-26 01:22:05 +0200

    Chat Server

--- .github/run_server.sh
+++ .github/run_server.sh
@@ -4,4 +4,4 @@
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
 # Run the commands with the full path to the binaries
-setsid konsole -e "${SCRIPT_DIR}/../bin/Server" &
+bash -c "${SCRIPT_DIR}/../bin/Server"
