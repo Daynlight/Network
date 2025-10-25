e554acf3014bf1345b5965c69b51cce3ad4e3c67
daynlight <danielstodulski000@gmail.com>
2025-10-25 13:01:23 +0200

    copy from labs

--- build/CMakeFiles/Run_All.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Run_All"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Run_All.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
