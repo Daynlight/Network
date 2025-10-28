0e5505874d3886e861dc69f9f06125ced0f7ecdd
daynlight <danielstodulski000@gmail.com>
2025-10-28 12:10:06 +0100

    netowkr done

--- Network/network.c
+++ Network/network.c
@@ -23,8 +23,8 @@ enum NetworkCodes network_client_init(struct network_client* network_socket, con
     return ERRORCODE;
 
   if ((network_socket->sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) < 0) {
-      freeaddrinfo(res);
-      return ERRORCODE;
+    freeaddrinfo(res);
+    return ERRORCODE;
   }
 
   network_socket->serv_addr = *(struct sockaddr_in*)res->ai_addr;
@@ -43,11 +43,11 @@ enum NetworkCodes network_client_init(struct network_client* network_socket, con
 
 enum NetworkCodes network_client_destroy(struct network_client* network_socket){
   if(network_socket->sock != -1)
-    #ifdef WIN32
-      closesocket(network_socket->sock);
-    #else
-      close(network_socket->sock);
-    #endif
+#ifdef WIN32
+    closesocket(network_socket->sock);
+#else
+    close(network_socket->sock);
+#endif
 
 #ifdef WIN32
   WSACleanup();
@@ -58,81 +58,80 @@ enum NetworkCodes network_client_destroy(struct network_client* network_socket){
 
 enum NetworkCodes network_client_connect(struct network_client* network_socket) {
 #ifdef WIN32
-    if (connect(network_socket->sock, (struct sockaddr *)&network_socket->serv_addr, sizeof(network_socket->serv_addr)) < 0) {
-        int err = WSAGetLastError();
-        if (err != WSAEWOULDBLOCK && err != WSAEINPROGRESS) {
-            return ERRORCODE; // real error
-        }
+  if (connect(network_socket->sock, (struct sockaddr *)&network_socket->serv_addr, sizeof(network_socket->serv_addr)) < 0) {
+    int err = WSAGetLastError();
+    if (err != WSAEWOULDBLOCK && err != WSAEINPROGRESS) {
+      return ERRORCODE;
+    };
+ 
+    fd_set writefds;
+    FD_ZERO(&writefds);
+    FD_SET(network_socket->sock, &writefds);
 
-        // Wait until the socket is writable or timeout
-        fd_set writefds;
-        FD_ZERO(&writefds);
-        FD_SET(network_socket->sock, &writefds);
+    struct timeval tv;
+    tv.tv_sec = CONNECT_RETRY_DELAY;
+    tv.tv_usec = 0;
 
-        struct timeval tv;
-        tv.tv_sec = CONNECT_RETRY_DELAY; // retry delay in seconds
-        tv.tv_usec = 0;
-
-        int sel = select(0, NULL, &writefds, NULL, &tv);
-        if (sel > 0 && FD_ISSET(network_socket->sock, &writefds)) {
-            return SUCCESS; // connected
-        } else {
-            return ERRORCODE; // timeout or select error
-        }
+    int sel = select(0, NULL, &writefds, NULL, &tv);
+    if (sel > 0 && FD_ISSET(network_socket->sock, &writefds)) {
+      return SUCCESS;
     } else {
-      u_long mode = 1;
-      ioctlsocket(network_socket->sock, FIONBIO, &mode);
-      return SUCCESS; // immediate success
+      return ERRORCODE;
     }
+  } else {
+    u_long mode = 1;
+    ioctlsocket(network_socket->sock, FIONBIO, &mode);
+    return SUCCESS;
+  };
 #else
-    unsigned int retry = 0;
-    while (retry <= MAX_CONNECT_RETRYS) {
-        if (connect(network_socket->sock, (struct sockaddr *)&network_socket->serv_addr, sizeof(network_socket->serv_addr)) < 0) {
-            retry++;
-            sleep(CONNECT_RETRY_DELAY);
-        } else
-            return SUCCESS;
-    }
-    return ERRORCODE;
+  unsigned int retry = 0;
+  while (retry <= MAX_CONNECT_RETRYS) {
+    if (connect(network_socket->sock, (struct sockaddr *)&network_socket->serv_addr, sizeof(network_socket->serv_addr)) < 0) {
+      retry++;
+      sleep(CONNECT_RETRY_DELAY);
+    } 
+    else
+      return SUCCESS;
+    };
+  return ERRORCODE;
 #endif
-}
+};
 
 int network_client_read(struct network_client *network_socket, char *buffer, const unsigned int buffer_size) {
-  int val;
-
 #ifdef WIN32
-  val = recv(network_socket->sock, buffer, buffer_size, 0);
+  int val = recv(network_socket->sock, buffer, buffer_size, 0);
   if(val == SOCKET_ERROR){
-      int err = WSAGetLastError();
-      if(err == WSAEWOULDBLOCK)
-          return NODATA;
-      else
-          return ERRORCODE;
-  }
+    int err = WSAGetLastError();
+    if(err == WSAEWOULDBLOCK)
+      return NODATA;
+    else
+      return ERRORCODE;
+  };
 #else
-  val = read(network_socket->sock, buffer, buffer_size);
+  int val = read(network_socket->sock, buffer, buffer_size);
   if(val < 0){
-      if(errno == EAGAIN || errno == EWOULDBLOCK)
-          return NODATA;
-      else
-          return ERRORCODE;
-  }
+    if(errno == EAGAIN || errno == EWOULDBLOCK)
+      return NODATA;
+    else
+      return ERRORCODE;
+  };
 #endif
 
   if(val == 0)
-      return DISCONNECT;
+    return DISCONNECT;
 
   return val;
 };
 
 enum NetworkCodes network_client_send(struct network_client *network_socket, char *buffer, const unsigned int max_message_size){
   if(buffer == NULL || strlen(buffer) == 0)
-    return NO_DATA;
+    return NODATA;
 
   if(strlen(buffer) > max_message_size)
     send(network_socket->sock, buffer, max_message_size, 0);
   else
     send(network_socket->sock, buffer, strlen(buffer), 0);
+    
   return SUCCESS;
 };
 
@@ -184,18 +183,18 @@ enum NetworkCodes network_server_init(struct network_server *network_socket, con
 enum NetworkCodes network_server_destroy(struct network_server *network_socket) {
   for(int i = 0; i < MAX_CLIENTS; i++)
     if(network_socket->client_sock[i] != -1)
-      #ifdef WIN32
-        closesocket(network_socket->client_sock[i]);
-      #else
-        close(network_socket->client_sock[i]);
-      #endif
+#ifdef WIN32
+      closesocket(network_socket->client_sock[i]);
+#else
+      close(network_socket->client_sock[i]);
+#endif
 
   if(network_socket->server_sock != -1)
-    #ifdef WIN32
-      closesocket(network_socket->server_sock);
-    #else
-      close(network_socket->server_sock);
-    #endif
+#ifdef WIN32
+    closesocket(network_socket->server_sock);
+#else
+    close(network_socket->server_sock);
+#endif
 
 #ifdef WIN32
   WSACleanup();
@@ -246,38 +245,40 @@ int network_server_find_free_socket(struct network_server *network_socket) {
 };
 
 int network_server_read(struct network_server *network_socket, unsigned int i, char *buffer, const unsigned int buffer_size){
-  int val;
-
 #ifdef WIN32
-    val = recv(network_socket->client_sock[i], buffer, buffer_size, 0);
-    if(val == SOCKET_ERROR){
-        int err = WSAGetLastError();
-        if(err == WSAEWOULDBLOCK)
-            return NODATA;
-        else
-            return ERRORCODE;
-    }
+  int val = recv(network_socket->client_sock[i], buffer, buffer_size, 0);
+  if(val == SOCKET_ERROR){
+    int err = WSAGetLastError();
+    if(err == WSAEWOULDBLOCK)
+      return NODATA;
+    else
+      return ERRORCODE;
+  };
 #else
-    val = read(network_socket->client_sock[i], buffer, buffer_size);
-    if(val < 0){
-        if(errno == EAGAIN || errno == EWOULDBLOCK)
-            return NODATA;
-        else
-            return ERRORCODE;
-    }
+  int val = read(network_socket->client_sock[i], buffer, buffer_size);
+  if(val < 0){
+    if(errno == EAGAIN || errno == EWOULDBLOCK)
+      return NODATA;
+    else
+      return ERRORCODE;
+  };
 #endif
 
-    if(val == 0)
-        return DISCONNECT;
+  if(val == 0)
+    return DISCONNECT;
 
-    return val;
+  return val;
 }
 
 enum NetworkCodes network_server_send(struct network_server *network_socket, const unsigned int i, char *buffer, const unsigned int max_message_size) {
+  if(strlen(buffer) == 0)
+    return NODATA;
+
   if(strlen(buffer) > max_message_size)
     send(network_socket->client_sock[i], buffer, max_message_size, 0);
   else
     send(network_socket->client_sock[i], buffer, strlen(buffer), 0);
+  
   return SUCCESS;
 };
 
@@ -298,8 +299,9 @@ enum NetworkCodes network_server_get_client_ip(struct network_server *network_so
 
   char client_ip[INET_ADDRSTRLEN];
   if (getpeername(network_socket->client_sock[i], (struct sockaddr *)&client_addr, &client_len) != 0) {
-      return ERRORCODE;
+    return ERRORCODE;
   }
+  
   inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, sizeof(client_ip));
 
   strncpy(buffer, client_ip, INET_ADDRSTRLEN - 1);
