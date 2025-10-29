9b9499b3fe75d0c6ef23749669eb05b34deecd36
daynlight <danielstodulski000@gmail.com>
2025-10-29 09:28:43 +0100

    CLient cleaner and safaer

--- Example/Chat/Client/client.c
+++ Example/Chat/Client/client.c
@@ -17,12 +17,15 @@ int running = 1;
 
 
 
-
 void sigint_handler(int sig){
   network_client_destroy(&network);
+  printf("Network destroyed!\n");
   running = 0;
   exit(EXIT_FAILURE);
 };
+
+
+
 
 #ifdef WIN32
 void set_stdin_nonblocking(void) {
@@ -42,8 +45,78 @@ void set_stdin_nonblocking(void) {
     fprintf(stderr, "SetConsoleMode() failed\n");
   }
 }
-#endif
 
+void client_get_input(){
+  if (_kbhit()) {
+    int ch = _getch();
+    if (ch == '\r' || ch == '\n') {
+      send_buffer[strlen(send_buffer)] = '\0';
+      if (strcmp(send_buffer, "exit") == 0) {
+        running = 0;
+        return;
+      };
+
+      char buffer[NAMESIZE + BUFFER_SIZE] = {0};
+      strcat(buffer, name);
+      strcat(buffer, send_buffer);
+
+      if(send_buffer == NULL || strlen(send_buffer) == 0 || send_buffer[0] == '\0')
+        printf("Can't send empty message\n");
+      else
+        network_client_send(&network, buffer, strlen(buffer));
+
+      printf("\n> ");
+      memset(send_buffer, 0, BUFFER_SIZE);
+    } 
+    else if (ch == 8) { // backspace
+      size_t len = strlen(send_buffer);
+      if (len > 0) {
+        send_buffer[len - 1] = '\0';
+        printf("\b \b");
+        fflush(stdout);
+      };
+    } 
+    else {
+      size_t len = strlen(send_buffer);
+      if (len < BUFFER_SIZE - 1)
+        send_buffer[len] = (char)ch, send_buffer[len + 1] = '\0';
+      printf("%c", ch);
+    };
+  };
+};
+
+void client_sleep(float time_seconds){
+  Sleep(time_seconds * 1000);
+};
+
+#else
+void client_get_input(){
+  if (fgets(send_buffer, BUFFER_SIZE, stdin) != NULL) {
+    send_buffer[strcspn(send_buffer, "\n")] = 0;
+    
+    if (strcmp(send_buffer, "exit") == 0) {
+      running = 0;
+      return;
+    };
+
+    char buffer[NAMESIZE + BUFFER_SIZE] = {0};
+    strcat(buffer, name);
+    strcat(buffer, send_buffer);
+
+    if(send_buffer == NULL || strlen(send_buffer) == 0 || send_buffer[0] == '\0')
+      printf("Can't send empty message\n");
+    else
+      network_client_send(&network, buffer, strlen(buffer));
+    
+    printf("> ");
+    memset(send_buffer, 0, BUFFER_SIZE);
+  };
+};
+
+void client_sleep(float time_seconds){
+  sleep(time_seconds);
+};
+#endif
 
 
 
@@ -66,13 +139,17 @@ int main(){
       break;
   };
 
-  if(network_client_connect(&network)){
-    printf("Can't connect to network!\n");
-    exit(EXIT_FAILURE);  
-  }
-  else{
-    printf("Connected to server!\n");
-  }
+  switch (network_client_connect(&network)){
+    case CONNECTERROR:
+      printf("Can't connect to network!\n");
+      network_client_destroy(&network);
+      printf("Network destroyed!\n");
+      exit(EXIT_FAILURE);
+      break;
+    default:
+      printf("Connected to server!\n");  
+  };
+
 
   printf("name: ");
   fgets(name, NAMESIZE -1, stdin);
@@ -80,75 +157,18 @@ int main(){
   strcat(name, " ");
   printf("logged as %s\n", name);
 
+
 #ifndef WIN32
   fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
 #endif
 
 
-
   printf("> ");
   while (running) {
-#ifdef _WIN32
-    if (_kbhit()) {
-      int ch = _getch();
-      if (ch == '\r' || ch == '\n') {
-        send_buffer[strlen(send_buffer)] = '\0';
-        if (strcmp(send_buffer, "exit") == 0) {
-          running = 0;
-          break;
-        };
-
-        char buffer[NAMESIZE + BUFFER_SIZE] = {0};
-        strcat(buffer, name);
-        strcat(buffer, send_buffer);
-
-        if(send_buffer == NULL || strlen(send_buffer) == 0 || send_buffer[0] == '\0')
-          printf("Can't send empty message\n");
-        else
-          network_client_send(&network, buffer, strlen(buffer));
-
-        printf("\n> ");
-        memset(send_buffer, 0, BUFFER_SIZE);
-      } 
-      else if (ch == 8) { // backspace
-        size_t len = strlen(send_buffer);
-        if (len > 0) {
-          send_buffer[len - 1] = '\0';
-          printf("\b \b");
-          fflush(stdout);
-        };
-      } 
-      else {
-        size_t len = strlen(send_buffer);
-        if (len < BUFFER_SIZE - 1)
-          send_buffer[len] = (char)ch, send_buffer[len + 1] = '\0';
-        printf("%c", ch);
-      };
-    };
-#else
-    if (fgets(send_buffer, BUFFER_SIZE, stdin) != NULL) {
-      send_buffer[strcspn(send_buffer, "\n")] = 0;
-      if (strcmp(send_buffer, "exit") == 0) {
-        running = 0;
-        break;
-      };
-
-      char buffer[NAMESIZE + BUFFER_SIZE] = {0};
-      strcat(buffer, name);
-      strcat(buffer, send_buffer);
-
-
-      if(send_buffer == NULL || strlen(send_buffer) == 0 || send_buffer[0] == '\0')
-        printf("Can't send empty message\n");
-      else
-        network_client_send(&network, buffer, strlen(buffer));
-      
-      printf("> ");
-      memset(send_buffer, 0, BUFFER_SIZE);
-    };
-#endif
+    client_get_input();
 
     int valread = network_client_read(&network, read_buffer, BUFFER_SIZE + NAMESIZE);
+    
     if (valread == DISCONNECT) {
       printf("\nServer closed the connection.\n");
       running = 0;
@@ -158,15 +178,13 @@ int main(){
       memset(read_buffer, 0, BUFFER_SIZE + NAMESIZE);
     };
 
-#ifdef _WIN32
-    Sleep(50);
-#else
-    sleep(0.05);
-#endif
+    client_sleep(0.05);
   };
 
 
   network_client_destroy(&network);
+  printf("Network destroyed!\n");
+
   return 0;
 };
 
