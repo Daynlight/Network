f43c85dee3659962d83749acd1c09696d684fa22
daynlight <danielstodulski000@gmail.com>
2025-10-30 20:21:27 +0100

    tests

--- Tests/UnitTests/compresion_tests.h
+++ Tests/UnitTests/compresion_tests.h
@@ -1,7 +1,14 @@
 #ifndef COMPRESSION_TESTS_H
 #define COMPRESSION_TESTS_H
 
+#include <stdio.h>
 
 
+
+
+
+void unittests_compression_run_all(){
+
+};
 
 #endif
--- Tests/UnitTests/tests.c
+++ Tests/UnitTests/tests.c
@@ -1,9 +1,10 @@
 #include "network_tests.h"
-
+#include "compresion_tests.h"
 
 
 int main(){
   unittests_network_run_all();
+  unittests_compression_run_all();
 
 
   return 0;
