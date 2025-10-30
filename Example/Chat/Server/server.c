13e61622c31c85c6800c9cf5d037f042ee6e98e3
daynlight <danielstodulski000@gmail.com>
2025-10-30 19:59:13 +0100

    server done

--- Example/Chat/Server/server.c
+++ Example/Chat/Server/server.c
@@ -59,15 +59,20 @@ int main() {
   init_clients(&clients);
 
   while (running) {
-    // connect 
-    int socket = network_server_listen(&network);
-    if(socket == ERRORCODE)
+    // connect
+    int socket = network_server_listen(&network); 
+    switch (socket){
+      case ERRORCODE:
       printf("Cant connect client!\n");
-    else if(socket >= SUCCESS){
+      break;
+    case NOCLIENT:
+      break;
+    default:
       char ip[INET_ADDRSTRLEN];
       network_get_client_ip(&socket, ip);
       add_client(&clients, socket);
       printf("Client connected ip: %s\n", ip);
+      break;
     };
 
     // broadcast
@@ -75,18 +80,21 @@ int main() {
       memset(buffer, 0, BUFFER_SIZE + NAMESIZE);
       if(clients.client_sock[i] > 0){
         int valread = network_server_read(&clients.client_sock[i], buffer,  BUFFER_SIZE + NAMESIZE);
-        if(valread > 0){
-          printf("%s\n", buffer);
-          network_server_broadcast(&network, clients.client_sock, clients.max_clients, i, buffer, BUFFER_SIZE + NAMESIZE);
-        }
-        else if(valread == DISCONNECT){
+        switch(valread){
+        case DISCONNECT:
           close(clients.client_sock[i]);
           delete_client(&clients, i);
           printf("Client disconnected\n");
+          break;
+        case NODATA:
+          break;
+        default:
+          printf("%s\n", buffer);
+          network_server_broadcast(&network, clients.client_sock, clients.max_clients, i, buffer, BUFFER_SIZE + NAMESIZE);
+          break;
         };
       };
     };
-    
     client_sleep(0.05);
   };
 
@@ -96,4 +104,4 @@ int main() {
   printf("Server destroyed!\n");
 
   return 0;
-}
+};
