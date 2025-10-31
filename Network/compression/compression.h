ddebe80acefeec893a38ff75edd3e365cbde1580
daynlight <danielstodulski000@gmail.com>
2025-10-31 08:45:16 +0100

    libs

--- Network/compression/compression.h
+++ Network/compression/compression.h
@@ -2,11 +2,6 @@
 #define COMPRESSION_H
 
 #include <stdio.h>
-#include <stdint.h>
-#include <string.h>
-
-#define WINDOW_SIZE 255  // look-back window
-#define MAX_LENGTH 255   // max match length
 
 void compression_rle_compress(char* buffer, char *outbuffer);
 void compression_rle_decompress(char* buffer, char *outbuffer);
