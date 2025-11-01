a7e17af96ce35db49fb54726b7517fb0e973ac0c
daynlight <danielstodulski000@gmail.com>
2025-11-01 16:29:59 +0100

    tin server and bug with /list prints >

--- Example/Macro.h
+++ Example/Macro.h
@@ -2,8 +2,8 @@
 #define MACRO_H
 
 #define PORT 9090
-// #define ADDR "tin-s498831.vm.wmi.amu.edu.pl"
-#define ADDR "127.0.0.1"
+#define ADDR "tin-s498831.vm.wmi.amu.edu.pl"
+// #define ADDR "127.0.0.1"
 
 #define BUFFER_SIZE 100
 #define NAMESIZE 25
