8fe7665c4425eec07c1b413c26ca373decd6fb20
daynlight <danielstodulski000@gmail.com>
2025-11-07 13:32:31 +0100

    dns name for gcd

--- Example/Macro.h
+++ Example/Macro.h
@@ -3,7 +3,7 @@
 
 #define PORT 9090
 // #define ADDR "tin-s498831.vm.wmi.amu.edu.pl"
-#define ADDR "34.116.226.18"
+#define ADDR "api.chat.daynlight.pl"
 // #define ADDR "127.0.0.1"
 
 #define BUFFER_SIZE 100
