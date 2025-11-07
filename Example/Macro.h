effa94922975ff58a636105e39b6027096fc0eb1
daynlight <danielstodulski000@gmail.com>
2025-11-07 16:32:18 +0100

    send request

--- Example/Macro.h
+++ Example/Macro.h
@@ -3,8 +3,8 @@
 
 #define PORT 9090
 // #define ADDR "tin-s498831.vm.wmi.amu.edu.pl"
-// #define ADDR "api.chat.daynlight.pl"
-#define ADDR "127.0.0.1"
+#define ADDR "api.chat.daynlight.pl"
+// #define ADDR "127.0.0.1"
 
 #define BUFFER_SIZE 100
 #define NAMESIZE 25
